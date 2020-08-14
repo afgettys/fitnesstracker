7726d608df49dba376c8902e5de21ce66e3016bb
Amanda Gettys <afgettys@gmail.com>
2020-08-14 16:00:14 -0400

    adding gitignore,app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+const PORT = process.env.PORT || 8080
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
